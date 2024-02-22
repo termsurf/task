322c57ed207c5af20e83b17ba6b832a7127ff0de
Lance Pollard <lp@elk.fm>
2024-02-21 21:13:40 -0800

    cleanup

--- test/index.ts
+++
@@ -1,8 +0,0 @@
-import node from '~/code/node'
-
-// const format = JSON.parse('"jpg"')
-
-// node.convert({
-//   input: { format: 'png', file: { path: './foo.png' } },
-//   output: { format: format, file: { path: './foo.jpg' } },
-// })
